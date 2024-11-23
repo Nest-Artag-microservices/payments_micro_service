c484eae1c8016f458062ce890e69446ba7125446
Artag/Chris <scristxyz@gmail.com>
2024-11-23 11:35:12 -0500

    act

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envs } from './config';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const logger = new Logger('payments-ms main');
   const app = await NestFactory.create(AppModule);
+  
+  app.useGlobalPipes(
+    new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true
+  }))
+
   await app.listen(envs.port);
   logger.log(`payments microservice listening on port ${envs.port}`);
 }
